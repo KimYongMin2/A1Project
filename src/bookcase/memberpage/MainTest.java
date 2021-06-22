a2f1641b42dbd1f21486ac9948a401a0e7152887
KimYongMin2 <kym960304@naver.com>
2021-06-22 14:13:56 +0900

    maintest

--- src/bookcase/memberpage/MainTest.java
+++ src/bookcase/memberpage/MainTest.java
@@ -6,8 +6,10 @@ import bookcase.manager.BookManager;
 public class MainTest {
     public static void main(String[] args) {
         Member member = new Member(10, "a", "a", "a", 10, "a", "a", 10);
-        new MemberPage(member).memberPageStart();
         BookManager bookManager = new BookManager();
         bookManager.bookManagerStart();
+        new MemberPage(member).memberPageStart();
+
+
     }
 }
