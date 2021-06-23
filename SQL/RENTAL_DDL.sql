c61f65ca855e5fb3a54c6b8c10e696ad4a4aec92
jiwon <xxxlikeag1_@naver.com>
2021-06-23 11:32:21 +0900

    SQL

--- SQL/RENTAL_DDL.sql
+++ SQL/RENTAL_DDL.sql
@@ -1,16 +1,21 @@
 
+
 /* 대여 */
-CREATE TABLE Rental (
-	RentalCode NUMBER(4) CONSTRAINT RENTAL_CODE_PK PRIMARY KEY, /* 대여코드 */
-	RentalDate DATE DEFAULT SYSDATE CONSTRAINT RETAL_RENTALDATE_NN NOT NULL, /* 대여날짜 */
-	RetrunDate DATE DEFAULT SYSDATE+7 CONSTRAINT RETAL_RETRUNDATE_NN NOT NULL, /* 반납날짜 */
-	MemberCode NUMBER(4) NOT NULL constraint RENTAL_MemberCode_fk REFERENCES MEMBER (MemberCode), /* 회원코드 */
-	BookCode NUMBER(4) NOT NULL constraint RENTAL_BookCode_fk REFERENCES BOOK (BookCode) /* 도서코드 */
+Create Table Rental (
+	Rentalcode Number(4) Constraint Rental_Code_Pk Primary Key, /* 대여코드 */
+	Rentaldate Date Default Sysdate Constraint Retal_Rentaldate_Nn Not Null, /* 대여날짜 */
+	Returndate Date Default Sysdate+7 Constraint Retal_Returndate_Nn Not Null, /* 반납날짜 */
+	Membercode Number(4) Not Null Constraint Rental_Membercode_Fk References Member (Membercode), /* 회원코드 */
+	Bookcode Number(4) Not Null Constraint Rental_Bookcode_Fk References Book (Bookcode) /* 도서코드 */
 );
 
 COMMENT ON TABLE Rental IS '대여';
 COMMENT ON COLUMN Rental.RentalCode IS '대여코드';
 COMMENT ON COLUMN Rental.RentalDate IS '대여날짜';
-COMMENT ON COLUMN Rental.RetrunDate IS '반납날짜';
+COMMENT ON COLUMN Rental.ReturnDate IS '반납날짜';
 COMMENT ON COLUMN Rental.MemberCode IS '회원코드';
 COMMENT ON COLUMN Rental.BookCode IS '도서코드';
+
+drop table RENTAL;
+
+commit;
