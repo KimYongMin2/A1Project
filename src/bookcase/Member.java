592591b51fc799b589a472dcc83b012e46121b76
jiwon <xxxlikeag1_@naver.com>
2021-06-23 16:46:46 +0900

    Update Member.java

--- src/bookcase/Member.java
+++ src/bookcase/Member.java
@@ -8,88 +8,67 @@ public class Member {
     private int age;
     private String phoneNum;
     private String email;
-    private int point;
-
-    public Member(int memberCode, String id, String passWord, String mName, int age, String phoneNum, String email, int point) {
-        this.memberCode = memberCode;
-        this.id = id;
-        this.passWord = passWord;
-        this.mName = mName;
-        this.age = age;
-        this.phoneNum = phoneNum;
-        this.email = email;
-        this.point = point;
-    }
-
-    public int getMemberCode() {
-        return memberCode;
-    }
-
-    public void setMemberCode(int memberCode) {
-        this.memberCode = memberCode;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    public String getmName() {
-        return mName;
-    }
-
-    public void setmName(String mName) {
-        this.mName = mName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPoint() {
-        return point;
-    }
-
-    public void setPoint(int point) {
-        this.point = point;
-    }
-
+    
+    
+	public int getMemberCode() {
+		return memberCode;
+	}
+	public void setMemberCode(int memberCode) {
+		this.memberCode = memberCode;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Member(int memberCode, String id, String passWord, String mName, int age, String phoneNum, String email) {
+		this.memberCode = memberCode;
+		this.id = id;
+		this.passWord = passWord;
+		this.mName = mName;
+		this.age = age;
+		this.phoneNum = phoneNum;
+		this.email = email;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "Member [memberCode=" + memberCode + ", id=" + id + ", passWord=" + passWord + ", mName=" + mName
-				+ ", age=" + age + ", phoneNum=" + phoneNum + ", email=" + email + ", point=" + point + "]";
+				+ ", age=" + age + ", phoneNum=" + phoneNum + ", email=" + email + "]";
 	}
-    
+
+
     
 }
